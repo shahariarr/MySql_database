311c72529622d4254d272da8814068206d308e63
Md. Shahariar Rahman <99550336+shahariarr@users.noreply.github.com>
2024-10-26 00:20:38 +0600

    first commit

---
+++ Part1.sql
@@ -0,0 +1,6 @@
+-- How to Write Insert Query
+INSERT INTO `Employees`
+(`firstName`,`lastName`,`City`,`Phone`,`Email`,`Salary`)
+VALUES
+('Rabbil','Hasan','Dhaka','01785388919','rabbil@idlc.com',1000)
+
